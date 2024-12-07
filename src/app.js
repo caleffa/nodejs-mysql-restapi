2b83a81c62ce8415d029858b85726fc9fccaa81e
caleffa <caleffa@gmail.com>
2024-12-07 18:25:01 -0300

    Add files via upload
    Manualmente

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
-import express from 'express'
-import employeesRoutes from './routes/employees.routes.js'
-import indexRoutes from './routes/index.routes.js'
+import express from "express";
+import employeesRoutes from "./routes/employees.routes.js";
+import categoriesRoutes from "./routes/categories.routes.js";
+import indexRoutes from "./routes/index.routes.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(indexRoutes)
-app.use('/api/v1',employeesRoutes)
+app.use(indexRoutes);
+app.use("/api/v1", employeesRoutes);
+app.use("/api/v1", categoriesRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'EndPoint not found'
-    })
-})
+  res.status(404).json({
+    message: "EndPoint not found",
+  });
+});
 
 export default app;
